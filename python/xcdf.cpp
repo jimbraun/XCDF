0a4aba56a723ab200fc49a9e95581f70e1b582c0
Maximilian Nöthe <maximilian.noethe@tu-dortmund.de>
2022-06-13 15:33:53 +0200

    Start working on new pybind11 python bindings

---
+++ python/xcdf.cpp
@@ -0,0 +1,37 @@
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+#include <sstream>
+
+#include "xcdf/XCDFFile.h"
+
+namespace py = pybind11;
+
+std::string getVersion() {
+    std::stringstream ss;
+    ss << XCDF_MAJOR_VERSION
+        << "." << XCDF_MINOR_VERSION
+        << "." << XCDF_PATCH_VERSION;
+    return ss.str();
+}
+
+PYBIND11_MODULE(xcdf, m) {
+    m.doc() = "Python Bindings for XCDF";
+    m.attr("__version__") = getVersion();
+
+    py::class_<XCDFFile>(m, "File")
+        .def(py::init<const char*, const char*>())
+        .def("__len__", &XCDFFile::GetEventCount)
+        .def("__in__", &XCDFFile::HasField)
+        .def_property_readonly("version", &XCDFFile::GetVersion)
+        .def_property_readonly("n_fields", &XCDFFile::GetNFields)
+        .def_property_readonly("is_simple", &XCDFFile::IsSimple)
+        .def_property_readonly("comments", [](XCDFFile& self) {
+            std::vector<std::string> comments;
+            comments.reserve(self.GetNComments());
+            for (auto it = self.CommentsBegin(); it != self.CommentsEnd(); it++) {
+                comments.push_back(*it);
+            }
+            return comments;
+        })
+        ;
+}
