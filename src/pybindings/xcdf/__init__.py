b1130ae0e6d9c0d7110be950010c8b2139ca7a9b
Michele Peresano <michele.peresano@cea.fr>
2023-05-21 01:54:28 +0200

    Update xcdf Python3 module
    - expose some basic API
    - add more docstrings

--- src/pybindings/xcdf/__init__.py
+++ src/pybindings/xcdf/__init__.py
@@ -1,5 +1,7 @@
 """
-Python bindings for XCDF using pybind11
+Python bindings for XCDF using pybind11.
 """
 
 from .xcdf import File, __version__, write_test_file
+
+__all__ = ["File", "__version__", "write_test_file"]
